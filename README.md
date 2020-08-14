9dc33127c9ab145250c6479afc9265742a16de8e
MarekJaskolski <marekjaskolski46@gmail.com>
2020-08-14 18:04:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-dvbat8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-dvbat8)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  blackDiv = 'black';
+  yellowDiv = 'yellow';
+  colorSwitch = 'green';
+  text = 'Wpisz coś';
+  htmlText =
+    '<h2>Anglar jest fajny</h2><blockquote>Ale trzeba się dużo uczyć</blockquote>';
+  isDisabled = true;
+  color = "red";
+  border = "1px solid white"
+}
